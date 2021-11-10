1313f37cf2a8be5750fd0cec0caee28897697769
aoimaru <51444995+aoimaru@users.noreply.github.com>
2021-11-10 23:04:33 +0900

    ハッシュ値での検索実装

--- main.py
+++ main.py
@@ -44,33 +44,32 @@ def main():
     file_paths = [comp for comp in glob.glob(PYTHON_PROJECT, recursive=True) if os.path.isfile(comp) if comp.endswith("Dockerfile")]
 
     results = {}
-    query = ['apt-get', 'install', '-y', '--no-install-recommends', 'uuid-dev']
+    query = ['apt-get', 'install', '-y', '--no-install-recommends', "ca-certificates"]
     # query = ["rm", "-rf"]
-    # hash_query = Hash.execute(query)
-    # query = [cnt for cnt in hash_query]
+    hash_query = Hash.execute(query)
+    query = [cnt for cnt in hash_query]
     print(query)
+    data = []
+    maps = {}
     for file_path in file_paths:
         model = Model(file_path)
         shells = model.shells
         for shell in shells:
-            # shell_dict = Hash.execute(shell)
-            # data = [cnt for cnt in shell_dict]
-            # print(data)
-            result = Zlib.ncd(query, shell)
-            print(result, ":", shell)
-            results[" ".join(shell)] = result
+            shell_dict = Hash.execute(shell)
+            data.append([cnt for cnt in shell_dict])
+            for key, value in shell_dict.items():
+                if not key in maps:
+                    maps[key] = value
     
-    results = sorted(results.items(), key=lambda x:x[1])
-    for result in results[:10]:
-        print(result)
-                
-    
-    
-    # for comp in data:
-    #     print(data)
-        # result = Zlib.ncd(query, data)
-        # print(result)
+    for shell_hash in data:
+        if query[-1] in shell_hash:
+            shell = [maps[sh] for sh in shell_hash]
+            print(shell)
+
             
+
+
+
             
 
 
